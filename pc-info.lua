473862b47c787ca3bb6ecc363e63c29ef2abf6a9
Wolfgang <68905084+Suppan@users.noreply.github.com>
2022-06-22 18:10:40 +0200

    Update pc-info.lua

--- pc-info.lua
+++ pc-info.lua
@@ -6,7 +6,7 @@ if(#arg > 0) then
     local set = arg
     print("chord: " .. table.concat(arg, " "))
     print("name: " .. pc_info(set,"set"))
-    print("prime: " .. pc_info(set,"prime") .. " iv:" .. pc_info(set,"vector"))
+    print("prime: " .. pc_info(set,"prime") .. " iv: " .. pc_info(set,"vector"))
 end
 
 --enter in terminal: lua pc-info.lua 60 64 55
