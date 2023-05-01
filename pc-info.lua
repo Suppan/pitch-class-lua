acdd02670d93c1291c3c6e1cdc58a486b860a49a
Wolfgang <68905084+Suppan@users.noreply.github.com>
2023-05-01 11:50:17 +0200

    Update pc-info.lua

--- pc-info.lua
+++ pc-info.lua
@@ -1,9 +1,9 @@
 #!/usr/bin/env lua
-require 'pc-functions'
+local pc = require 'pc-functions'
 
 if(#arg > 0) then
     local chord = arg
-    local normal,name,prime,vector = pc_info(chord)
+    local normal,name,prime,vector = pc.info(chord)
     print("midi: " .. table.concat(chord," "))
     print("normal: " .. table.concat(normal," "))
     print("prime: " .. prime)
